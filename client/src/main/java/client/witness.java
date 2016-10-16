f4d5214e67a978d6b003e806283546525eeba476
khaoulaIss <khaoula.issaoui@esprit.tn>
2016-10-16 16:07:34 +0200

    Rectifications
    Signed-off-by: khaoulaIss <khaoula.issaoui@esprit.tn>

--- client/src/main/java/client/witness.java
+++ client/src/main/java/client/witness.java
@@ -10,15 +10,15 @@ import entities.WitnessCard;
 public class witness {
 	public static void main(String[] args) throws NamingException {
 		Context context = new InitialContext();
-		WitnessCardServiceRemote wsr = (WitnessCardServiceRemote) context
-				.lookup("witnessbook-ear/witnessbook-ejb/WitnessCardService!Service.WitnessCardServiceRemote");
+		//WitnessCardServiceRemote wsr = (WitnessCardServiceRemote) context
+				//.lookup("witnessbook-ear/witnessbook-ejb/WitnessCardService!Service.WitnessCardServiceRemote");
 		WitnessCard wc = new WitnessCard();
 		wc.setCardId(1);
 		wc.setCardName("TopHappiness");
 		wc.setCardAddress("lac1");
 		wc.setCardDescription("good");
 		wc.setCardFile("scr:\\...");
-		wsr.Addwcs(wc);
+		//wsr.Addwcs(wc);
 
 	}
 
